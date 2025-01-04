fcf4b413b3ce0f2947c1a3f9b6fdfe667b786392
amillarwilson <160059289+amillarwilson@users.noreply.github.com>
2025-01-04 16:11:52 +0000

    code started

--- graphical_interface_handler.py
+++ graphical_interface_handler.py
@@ -1,3 +1,13 @@
 """
 contains the code to handle the graphical interface (TKINTER)
+
+plan:
+have multiple panels, so at the top you enter N bodies and then N boxes populate with spaces for
+masses, positions, and velocities
 """
+
+import tkinter as tk
+
+gravitational_system_config_window = tk.Tk()
+instructions = tk.Label(text="Please enter the number of gravitational bodies you want to simulate:")
+instructions.pack()
