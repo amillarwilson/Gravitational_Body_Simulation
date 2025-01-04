c321b732ffdcf248afa2a789f19e380f0bd94b9a
amillarwilson <millarwilsona@gmail.com>
2025-01-04 12:18:30 +0000

    refactored into system class but not bug fixed

--- modules.py
+++ modules.py
@@ -14,66 +14,6 @@ from matplotlib.animation import FuncAnimation
 #set G
 G = 6.6743e-11
 
-#calculate gravitational force between bodies
-def gravitational_force(mass1, mass2, pos1, pos2):
-    """
-    This function takes in the mass and position of bodies and uses F=Gmm/r^2
-    to calculate gravitational force between bodies
-    """
-    #distance between bodies
-    r = np.linalg.norm(pos2-pos1)
-
-    # Adding a check to avoid division by zero
-    if r < 1e-6:
-        return np.zeros_like(pos1)
-
-    #force
-    f_magnitude = G * (mass1 * mass2) / (r**2)
-    f_direction = (pos2-pos1) / r
-    force = f_magnitude * f_direction
-    return force
-
-#update body velocities and positions
-
-def position_update(bodies, dt):
-    """
-    This function takes in a list of bodies and dt, the integral timestep
-    using the RK4 method for integration here as it affords higheraccuracy than the Eular method
-    """
-    num_bodies = len(bodies)
-
-    for i in range(num_bodies):
-        #initialise force - 2d simulation
-        total_force = np.zeros(2)
-
-        #force calculation
-        for j in range(num_bodies):
-            #handle case where forces are calculated on the same object
-            if i !=j:
-                force = gravitational_force(bodies[i].mass, bodies[j].mass, bodies[i].pos, bodies[j].pos)
-                total_force += force
-        
-        #calculate acceleration (F=ma, rearrange to a=F/m)
-        acceleration = total_force / bodies[i].mass
-
-        #implement RK4 method
-        k1v = acceleration * dt
-        k1x = bodies[i].vel * dt
-
-        k2v = gravitational_force(bodies[i].mass, bodies[i].mass, bodies[i].pos + k1x / 2, bodies[i].pos + k1x / 2) / bodies[i].mass * dt
-        k2x = (bodies[i].vel + k1v / 2) * dt
-
-        k3v = gravitational_force(bodies[i].mass, bodies[i].mass, bodies[i].pos + k2x / 2, bodies[i].pos + k2x / 2) / bodies[i].mass * dt
-        k3x = (bodies[i].vel + k2v / 2) * dt
-
-        k4v = gravitational_force(bodies[i].mass, bodies[i].mass, bodies[i].pos + k3x, bodies[i].pos + k3x) / bodies[i].mass * dt
-        k4x = (bodies[i].vel + k3v) * dt
-
-        #update velocity and position using weghted average
-        bodies[i].vel += (k1v + 2 * k2v + 2 * k3v + k4v) /6
-        bodies[i].pos += (k1x + 2 * k2x + 2 * k3x + k4x) /6
-
-
 #create class which represents a planetary body
 class Body:
     """
@@ -85,124 +25,217 @@ class Body:
         self.mass = mass
         self.pos = pos
         self.vel = vel
-    
-#function to initialise random bodies
-def initialise_random_bodies(num_bodies):
+
+class System:
     """
-    a function which takes in the number of bodies and
-    instantiates a Body
-    """
-    bodies = []
-    for _ in range(num_bodies):
-        #random mass within a reasonable range
-        mass = np.random.uniform(1e9, 1e12)
-        pos = np.random.rand(2) #2D space
-        vel = np.zeros(2) #initial velocity of 0, may be customisable in later updates
-        bodies.append(Body(mass, pos, vel))
-    return bodies
-
-#function to initialise an unstable eliptical binary system
-def initialise_binary_system():
-    mass_body1 = 2e9  # Mass of the first body
-    mass_body2 = 2e10  # Mass of the second body
-
-    # Positions and velocities for a stable binary system
-    pos_body1 = np.array([0.2, 0.5])
-    vel_body1 = np.array([0.3, 0.5])
-
-    pos_body2 = np.array([0.5, 0.5])
-    vel_body2 = np.array([0.0001, 0.0001])
-
-    body1 = Body(mass_body1, pos_body1, vel_body1)
-    body2 = Body(mass_body2, pos_body2, vel_body2)
-
-    return [body1, body2]
-
-def initialise_custom_bodies(N, masses, poses, vels):
-    #A function to intialise custom systems - default is binary system
-
-    bodies=[]
-
-    for i in range(0,N-1):
-        bodies.append(Body(masses[i],poses[i],vels[i]))
-    
-    return bodies
-
-
-def plot_system(masses=[2e9,2e10], poses=[np.array([0.2, 0.5]),np.array([0.5, 0.5])], 
-                vels=[np.array([0.3, 0.5]), np.array([0.0001, 0.0001])], N=2, step=0.01,
-                plot_lims=[[None,None],[None,None]]):
-    """
-    A function to plot the system caluclated above.
-    inputs: 
-    N - number of bodies,
-    dt - integration step
-    plot_lims - plot limits
+    gravitational system class
+    instantiates bodies based on user settings (pre-set system or custom)
     """
 
-    #initialise bodies
-    bodies = initialise_custom_bodies(N, masses, poses, vels)
-
-    #initialise plot
-    fig, ax = plt.subplots()
-    scatter = ax.scatter([body.pos[0] for body in bodies], [body.pos[1] for body in bodies],
-                        s=[(body.mass/1e9) for body in bodies])
     
-    #set X,Y limits - if no limits given, set dynamically by max X and Y position
-    if plot_lims==[[None,None],[None,None]]:
-        ax.set_xlim(min(body.pos[0] for body in bodies) - 0.1, max(body.pos[0] for body in bodies) + 0.1)
-        ax.set_ylim(min(body.pos[1] for body in bodies) - 0.1, max(body.pos[1] for body in bodies) + 0.1)
+    def __init__(self, num_bodies = 3, system_type = "binary"):
+        if type(num_bodies) is int:
+            self.num_bodies = num_bodies
+        else:
+            TypeError("Number of bodies must be an integer")
 
-    else:
-        ax.set_xlim(plot_lims[0][0], plot_lims[0][1])
-        ax.set_xlim(plot_lims[1][0], plot_lims[1][1])
+        valid_system_types = ["random", "binary", "custom"]
 
+        if system_type.lower() in valid_system_types:
+            self.system_type = system_type.lower()
 
-    #set title
-    ax.set_title(f'{N} gravitational bodies interacting')
+            if self.system_type == "random":
+                #function to initialise random bodies
+                def initialise_random_bodies(num_bodies):
+                    """
+                    a function which takes in the number of bodies and
+                    instantiates a Body
+                    """
+                    self.bodies = []
+                    for _ in range(num_bodies):
+                        #random mass within a reasonable range
+                        mass = np.random.uniform(1e9, 1e12)
+                        pos = np.random.rand(2) #2D space
+                        vel = np.zeros(2) #initial velocity of 0, may be customisable in later updates
+                        self.bodies.append(Body(mass, pos, vel))
+                
+                initialise_random_bodies(self.num_bodies)
 
-    #add colourbar
-    cbar = plt.colorbar(scatter, ax=ax, label='Velocity Magnitude')
+            elif self.system_type == "binary":
+                #function to initialise an unstable eliptical binary system
+                def initialise_binary_system():
+                    mass_body1 = 2e9  # Mass of the first body
+                    mass_body2 = 2e10  # Mass of the second body
+
+                    # Positions and velocities for a stable binary system
+                    pos_body1 = np.array([0.2, 0.5])
+                    vel_body1 = np.array([0.3, 0.5])
+
+                    pos_body2 = np.array([0.5, 0.5])
+                    vel_body2 = np.array([0.0001, 0.0001])
+
+                    body1 = Body(mass_body1, pos_body1, vel_body1)
+                    body2 = Body(mass_body2, pos_body2, vel_body2)
+
+                    self.bodies = [body1, body2]
+                
+                initialise_binary_system()
     
-    #set integration step
-    dt = step
+            """
+            elif self.system_type == "custom":
+                    def initialise_custom_bodies(N, masses, poses, vels):
+                    #A function to intialise custom systems - default is binary system
 
-    #animate
-    animation = FuncAnimation(fig, update, frames = 100, interval = 50, blit = True)
+                    bodies=[]
+
+                    for i in range(0,N-1):
+                        bodies.append(Body(masses[i],poses[i],vels[i]))
+                    
+                    return bodies
+            
+                    #TODO: create the custom API
+            """
+            print("System initialised")
+
+        else:
+            TypeError(f"System type must be one of {valid_system_types}")
+
+        #system initialised - calculating forces
+
+        #calculate gravitational force between bodies
+    def gravitational_force(self, mass1, mass2, pos1, pos2):
+        """
+        This function takes in the mass and position of bodies and uses F=Gmm/r^2
+        to calculate gravitational force between bodies
+        """
+        #distance between bodies
+        r = np.linalg.norm(pos2-pos1)
+
+        # Adding a check to avoid division by zero
+        if r < 1e-6:
+            return np.zeros_like(pos1)
+
+        #force
+        f_magnitude = G * (mass1 * mass2) / (r**2)
+        f_direction = (pos2-pos1) / r
+        force = f_magnitude * f_direction
+        
+        return(force)
+
+    #update body velocities and positions
+    def position_update(self, bodies, dt):
+        """
+        This function takes in a list of bodies and dt, the integral timestep
+        using the RK4 method for integration here as it affords higheraccuracy than the Eular method
+        """
+        num_bodies = len(bodies)
+
+        for i in range(num_bodies):
+            #initialise force - 2d simulation
+            total_force = np.zeros(2)
+
+            #force calculation
+            for j in range(num_bodies):
+                #handle case where forces are calculated on the same object
+                if i !=j:
+                    force = self.gravitational_force(bodies[i].mass, bodies[j].mass, bodies[i].pos, bodies[j].pos)
+                    total_force += force
+            
+            #calculate acceleration (F=ma, rearrange to a=F/m)
+            acceleration = total_force / bodies[i].mass
+
+            #implement RK4 method
+            k1v = acceleration * dt
+            k1x = bodies[i].vel * dt
+
+            k2v = self.gravitational_force(bodies[i].mass, bodies[i].mass, bodies[i].pos + k1x / 2, bodies[i].pos + k1x / 2) / bodies[i].mass * dt
+            k2x = (bodies[i].vel + k1v / 2) * dt
+
+            k3v = self.gravitational_force(bodies[i].mass, bodies[i].mass, bodies[i].pos + k2x / 2, bodies[i].pos + k2x / 2) / bodies[i].mass * dt
+            k3x = (bodies[i].vel + k2v / 2) * dt
+
+            k4v = self.gravitational_force(bodies[i].mass, bodies[i].mass, bodies[i].pos + k3x, bodies[i].pos + k3x) / bodies[i].mass * dt
+            k4x = (bodies[i].vel + k3v) * dt
+
+            #update velocity and position using weghted average
+            bodies[i].vel += (k1v + 2 * k2v + 2 * k3v + k4v) /6
+            bodies[i].pos += (k1x + 2 * k2x + 2 * k3x + k4x) /6
+
+    def plot_system(self, masses=[2e9,2e10], poses=[np.array([0.2, 0.5]),np.array([0.5, 0.5])], 
+                    vels=[np.array([0.3, 0.5]), np.array([0.0001, 0.0001])], N=2, step=0.01,
+                    plot_lims=[[None,None],[None,None]]):
+        """
+        A function to plot the system caluclated above.
+        inputs: 
+        N - number of bodies,
+        dt - integration step
+        plot_lims - plot limits
+        """
+
+        #initialise bodies
+
+        #initialise plot
+        fig, ax = plt.subplots()
+        scatter = ax.scatter([body.pos[0] for body in self.bodies], [body.pos[1] for body in self.bodies],
+                            s=[(body.mass/1e9) for body in self.bodies])
+        
+        #set X,Y limits - if no limits given, set dynamically by max X and Y position
+        if plot_lims==[[None,None],[None,None]]:
+            ax.set_xlim(min(body.pos[0] for body in self.bodies) - 0.1, max(body.pos[0] for body in self.bodies) + 0.1)
+            ax.set_ylim(min(body.pos[1] for body in self.bodies) - 0.1, max(body.pos[1] for body in self.bodies) + 0.1)
+
+        else:
+            ax.set_xlim(plot_lims[0][0], plot_lims[0][1])
+            ax.set_xlim(plot_lims[1][0], plot_lims[1][1])
 
 
-    #show animation
-    plt.show()
+        #set title
+        ax.set_title(f'{N} gravitational bodies interacting')
 
-    #clear bodies
-    bodies = []
+        #add colourbar
+        cbar = plt.colorbar(scatter, ax=ax, label='Velocity Magnitude')
+        
+        #set integration step
+        dt = step
+
+        #animate
+        animation = FuncAnimation(fig, update, frames = 100, interval = 50, blit = True)
 
 
-#update plot
-def update(frame):
-    """
-    Function which runs position_update and updates the plot and colour
-    """
-    position_update(bodies, dt)
-    scatter.set_offsets(np.array([[body.pos[0], body.pos[1]] for body in bodies]))
+        #show animation
+        plt.show()
+
+        #clear bodies
+        self.bodies = []
+
+
+    #update plot
+    def update(self, frame):
+        """
+        Function which runs position_update and updates the plot and colour
+        """
+        self.position_update(bodies, dt)
+        scatter.set_offsets(np.array([[body.pos[0], body.pos[1]] for body in self.bodies]))
+        
+        #update colour
+        #get absolute vels
+        vel_abs = [np.linalg.norm(body.vel) for body in self.bodies]
+        
+        # Update scatter plot with new colormap based on absolute velocity
+        scatter.set_array(vel_abs)
+        scatter.set_cmap('jet')  # Set the colormap
+
+        #update colourbar limits to ensure distinct colours
+        #I have set this up more dynamically previously, but that makes N<3 body simulation colour schemes uninformative 
+        scatter.set_clim(vmin=0, vmax=2)
+
+
+        return [scatter]
     
-    #update colour
-    #get absolute vels
-    vel_abs = [np.linalg.norm(body.vel) for body in bodies]
-    
-    # Update scatter plot with new colormap based on absolute velocity
-    scatter.set_array(vel_abs)
-    scatter.set_cmap('jet')  # Set the colormap
 
-    #update colourbar limits to ensure distinct colours
-    #I have set this up more dynamically previously, but that makes N<3 body simulation colour schemes uninformative 
-    scatter.set_clim(vmin=0, vmax=2)
+system_1 = System("random")
 
-
-    return [scatter]
-    
-
-plot_system()
+system_1.plot_system()
 
 
 #sections to move to main running file and possibly refactor or add to TKINTER to make it more accessible
