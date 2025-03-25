499e4709548e8c07a4ff858de5c811f806d78df0
amillarwilson <160059289+amillarwilson@users.noreply.github.com>
2025-03-25 14:06:21 +0000

    renamed orchestrator to main

--- main.py
+++ main.py
@@ -8,9 +8,10 @@ This file serves as the handler which imports both the GUI and system simulator 
 It runs the GUI, passes the output to the system simulator, and returns the graphic.
 """
 
+
 #import GUI and system simulator
 from graphical_interface_handler import GUI_coordinator
-
+import system_simulator
 #run GUI class
 
 if __name__ == "__main__":
@@ -22,3 +23,5 @@ if __name__ == "__main__":
         if selected_type == "custom" and test_system.requested_gravitational_system_data:
             print("Requested data:", test_system.requested_gravitational_system_data)
 #pass GUI output to system simulator
+
+test_system <- system_simulator.System()
